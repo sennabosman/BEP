4cebdcb322c47fd8bc0c65771b4abf2094094f11
sennabosman <sennabosman@gmail.com>
2022-11-29 14:59:16 +0100

    added parallel sweep searching pattern

--- missing_person.py
+++ missing_person.py
@@ -4,10 +4,11 @@ from mesa import Agent
 class MissingPerson(Agent):
     """A person that gets missing in the mountains."""
 
-    def __init__(self, position, depth, unique_id, georesq=False):
+    def __init__(self, position, depth, unique_id, georesq=False, path=False):
         super().__init__(position, depth)
         self.unique_id = unique_id
         self.position = position
         self.depth = depth
         self.found = False
         self.georesq = georesq
+        self.path = path
